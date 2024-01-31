873077eeffff61ed96c3e83c930f3b37cfcb476d
Adams009 <olaadam003@gmail.com>
2024-01-30 20:35:08 -0800

    0-simply_match_school.rb

--- 0-simply_match_school.rb
+++ 0-simply_match_school.rb
@@ -1,11 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(arg)
-  puts arg.match(/School\z/) ? "School$" : "$"
-end
-
-if ARGV.length == 1
-  match_school(ARGV[0])
-else
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-end
+puts ARGV[0].scan(/School/).join
