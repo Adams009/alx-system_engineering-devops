5b7a8b557f901ab9e289f65eea99cbce9b91093b
Adams009 <olaadam003@gmail.com>
2024-01-30 20:39:51 -0800

    1-repetition_token_0.rb

--- 1-repetition_token_0.rb
+++ 1-repetition_token_0.rb
@@ -1,11 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_repetition_token_0(arg)
-  puts arg.match(/hb{0,}tn/) ? arg : ""
-end
-
-if ARGV.length == 1
-  match_repetition_token_0(ARGV[0])
-else
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-end
+puts ARGV[0]scan(/hbt{2,5}n/).join
