489499defb6fe4d1fde2251dd629a1d1ae5d0078
Adams009 <olaadam003@gmail.com>
2024-03-24 05:52:09 -0700

    goo

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,11 +2,9 @@
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+  provider => 'pip3'
 }
 package { 'werkzeug':
   ensure   => '2.1.1',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+  provider => 'pip3'
 }
