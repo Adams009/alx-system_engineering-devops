5b31faecb938a1dc68552fc7504324097a0c4950
Adams009 <olaadam003@gmail.com>
2024-05-08 04:06:48 -0700

    good

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,13 +10,15 @@ def top_ten(subreddit):
     '''
         returns the top ten posts for a given subreddit
     '''
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Reddit API Test Client'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
-    else:
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
         print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
